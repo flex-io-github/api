e285687d6bf1e24f59f4f73cf16880e52913b5f5
Ronald Manzano <ngnl_rmanzano@hotmail.com>
2018-01-08 12:49:06 +1100

    Initial Dev Commit

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Entities;
+using WebApi.Enums;
+using WebApi.Interfaces;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi
+{
+    
+    
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        Entities.DataContext dbContext;
+
+        public EmployeesController(Entities.DataContext dbContext) => this.dbContext = dbContext;
+        // GET: api/values
+
+        [HttpGet]
+        public IEnumerable<employee> Get()
+        {
+            return dbContext.employees;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public employee Get(int id)
+        {
+            return dbContext.employees.Where(t => t.id == id).FirstOrDefault();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public employee Post([FromBody]employee value)
+        {
+            dbContext.employees.Add(value);
+            dbContext.SaveChanges();
+            return value;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public employee Put(int id, [FromBody]employee value)
+        {
+            var entity = dbContext.employees.Where(t => t.id == id).FirstOrDefault();
+            entity.given_name = value.given_name;
+            entity.surname = value.surname;
+            dbContext.SaveChanges();
+            return entity;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public employee Delete(int id)
+        {
+            var entity = dbContext.employees.Where(t => t.id == id).FirstOrDefault();
+            dbContext.employees.Remove(entity);
+            dbContext.SaveChanges();
+            return entity;
+        }
+    }
+}
---
+++ Entities/DataContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore; 
+
+namespace WebApi.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(GetConnectionString());
+        }
+
+        private string GetConnectionString()
+        {
+            const string server = "localhost";
+            const string databaseName = "ngnl_dev";
+            const string userId = "sa";
+            const string databasePassword = "masterkey";
+            const string serverPort = "2000";
+
+            return $"Server={server};" +
+                    $"database={databaseName};" +
+                    $"User Id={userId};" +
+                    $"Password={databasePassword};" +
+                    $"Port={serverPort};" +
+                    $"Integrated Security=true;" +
+                    $"pooling=true;";
+
+        }
+
+        public DbSet<Entities.employee> employees { get; set; }
+        public DbSet<Entities.auth_user> auth_users { get; set; }
+        public DbSet<Entities.auth_user_role> auth_user_roles { get; set; }
+        public DbSet<Entities.Lookup> Lookups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<employee>().ToTable("employees");
+           
+        }
+    }
+}
